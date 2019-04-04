394927037b8613a8e33d379b0974d0b8507299a6
Mario Camara Ramil <36202396+kyzo98@users.noreply.github.com>
2019-04-04 13:30:11 +0200

    F

--- AA4/glframework/code/src/physics.cpp
+++ AA4/glframework/code/src/physics.cpp
@@ -41,7 +41,7 @@ namespace Mesh {
 	extern void drawMesh();
 }
 namespace Fiber {
-extern const int numVerts;
+	extern const int numVerts;
 	extern void updateFiber(float* array_data);
 	extern void drawFiber();
 }
@@ -50,23 +50,108 @@ namespace Cube {
 	extern void drawCube();
 }
 
-struct RigidSphere {
-	glm::vec3 x, v, P, L;
-	glm::mat3 w, I, Ibody, R;
+//ADDITIONAL FUNCTIONS
+//Function that generates a random float between two numbers
+float RandomFloat(float a, float b)
+{
+	float random = ((float)rand()) / (float)RAND_MAX;
+	float diff = b - a;
+	float r = random * diff;
+	return a + r;
+}
+
+//Function that returns the distance between a plane and a point
+float distancePlanePoint(const glm::vec3& n, const float& d, const glm::vec3& p)
+{
+	float up = n.x * p.x + n.y * p.y + n.z * p.z + d;
+	float down = glm::sqrt(pow(n.x, 2) + pow(n.y, 2) + pow(n.z, 2));
+	return up / down;
+}
+
+//Function that returns the module of a given vector
+float vectorModule(const glm::vec3& A)
+{
+	return (glm::sqrt((glm::pow(A.x, 2)) + (glm::pow(A.y, 2)) + (glm::pow(A.z, 2))));
+}
+
+//Function that returns the vector between two points
+glm::vec3 makeVector(const glm::vec3& A, const glm::vec3& B)
+{
+	return { (B.x - A.x), (B.y - A.y), (B.z - A.z) };
+}
+
+//Function that returns the crossProduct between two vectors
+glm::vec3 crossPorduct(const glm::vec3& A, const glm::vec3& B)
+{
+	return { ((B.z * A.y) - (B.y * A.z)), ((A.z * B.x) - (A.x * B.z)), ((A.x * B.y) - (B.x * A.y)) };
+}
+
+//Function that returns the normalized vector between two points
+glm::vec3 normalizeVector(const glm::vec3& A, const glm::vec3& B)
+{
+	glm::vec3 v{ (B.x - A.x), (B.y - A.y), (B.z - A.z) };
+	v = glm::normalize(v);
+	return v;
+}
+
+//Function that returns the dotProduct between two vectors
+float multVector(const glm::vec3& A, const glm::vec3& B)
+{
+	return (B.x * A.x) + (B.y * A.y) + (B.z * A.z);
+}
+
+
+struct Collider { 
+	virtual bool checkCollision(const glm::vec3& next_pos, float radius) = 0; 
+}; 
+struct PlaneCol : Collider {
+	glm::vec3 p1, p2, p3, p4;
+	glm::vec3 normal;
+	float d; 
+
+	PlaneCol(glm::vec3 _p1, glm::vec3 _p2, glm::vec3 _p3, glm::vec3 _p4) : p1(_p1), p2(_p2), p3(_p3), p4(_p4)
+	{
+		glm::vec3 v1 = makeVector(p1, p4);
+		glm::vec3 v2 = makeVector(p2, p3);
+
+		normal = glm::normalize(crossPorduct(v1, v2));
+		d = -(normal.x * p1.x + normal.y * p1.y + normal.z * p1.z);
+	}
+
+	bool checkCollision(const glm::vec3& next_pos, float radius) override {
+		return distancePlanePoint(normal, d, next_pos) <= radius;
+	}
+};
+
+struct RigidSphere :Collider{
+	glm::vec3 x, v, P, L, w;
+	glm::mat3 I, Ibody, R;
 	float radius;
 	float mass;
 
 	RigidSphere(const glm::vec3& initPos, const float& r, const float& m) : x(initPos), radius(r), mass(m)
-	{}
+	{
+		v = glm::vec3(RandomFloat(-2.f, 2.f), RandomFloat(-2.f, 2.f), RandomFloat(-2.f, 2.f));
+		P = mass * v;
+		w = glm::vec3(0, 0, 0);
+		R = glm::mat3(1, 0, 0, 0, 1, 0, 0, 0, 1);
+		Ibody = glm::mat3(2/5 * mass*radius*radius, 0, 0, 0, 2 / 5 * mass*radius*radius, 0, 0, 0, 2 / 5 * mass*radius*radius);
+		I = R * Ibody * R;
+		L = I * w;
+	}
 
 	void drawSphere()
 	{
 		Sphere::updateSphere(x, radius);
 		Sphere::drawSphere();
 	}
+
+	bool checkCollision(const glm::vec3& next_pos, float r) {
+		return vectorModule(next_pos - x) <= r + radius;
+	}
 };
 
-std::vector<RigidSphere> spheres {
+std::vector<RigidSphere> spheres{
 	{{1,1,1}, 2, 3},
 	{{5,2,1}, 1, 3},
 	{{-2,0.5,2}, 0.2, 3}
@@ -78,6 +163,23 @@ bool renderParticles = false;
 bool renderMesh = false;
 bool renderFiber = false;
 bool renderCube = false;
+
+void euler(float dt, RigidSphere& sph) {
+	sph.P += dt* sph.mass * gravity;
+	sph.
+	glm::mat3 Ii = sph.R * glm::inverse(sph.Ibody) * glm::transpose(sph.R);
+	sph.R = sph.R + dt * (glm::mat3(0, -sph.w.z, sph.w.y, sph.w.z, 0, -sph.w.x, -sph.w.y, sph.w.x, 0)* sph.R);
+	sph.I = sph.R * sph.Ibody * sph.R;
+	sph.w = Ii * sph.L;
+
+	
+	
+	
+	
+	
+	sph.v = sph.P / sph.mass;
+	sph.x = sph.x + sph.v*dt;
+}
 
 void renderPrims() {
 	Box::drawCube();
@@ -114,16 +216,16 @@ void Reset()
 void GUI() {
 	bool show = true;
 	ImGui::Begin("Physics Parameters", &show, 0);
-	{	
+	{
 		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-		
+
 		ImGui::Checkbox("Play simulation", &pause);
-		if(ImGui::Button("Reset"))
+		if (ImGui::Button("Reset"))
 			Reset();
 
 		ImGui::DragFloat3("Gravity Acceleration", &gravity.x, dragPrecision);
 		ImGui::DragFloat("Elastic Coefficient", &elasticCoefficient, dragPrecision, 0.0f, 1.0f);
-		
+
 		if (ImGui::TreeNode("Spheres"))
 		{
 			for (int i = 0; i < spheres.size(); i++)
@@ -140,21 +242,21 @@ void GUI() {
 		}
 
 	}
-	
+
 	ImGui::End();
 }
 
-void PhysicsInit() 
+void PhysicsInit()
 {
 
 }
 
-void PhysicsUpdate(float dt) 
+void PhysicsUpdate(float dt)
 {
 
 }
 
-void PhysicsCleanup() 
+void PhysicsCleanup()
 {
 
 }
